33ca25902c1e6a0d5e8568f531ddc47a8fc3e79a
emccallum <mccallum.ek@gmail.com>
2013-12-27 11:11:22 -0800

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject sentiment-analysis "0.1.0-SNAPSHOT"
+  :description "Twitter Sentiment Analysis"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main sentiment-analysis.core)
